8437cfb3146c5b6c306d532a42e1ad999d9e1759
Aster <galaster@foxmail.com>
2020-02-24 19:51:49 +0800

    ⚗ Support mathjax

---
+++ js/init.js
@@ -0,0 +1,25 @@
+console.log = function () { }
+
+window.MathJax = {
+    options: {
+        skipHtmlTags: [
+            'script', 'noscript', 'style', 'textarea', 'pre',
+            'code', 'annotation', 'annotation-xml'
+        ],
+        includeHtmlTags: {
+            br: ''
+        },
+    },
+    tex: {
+        inlineMath: [['$', '$']],
+        displayMath: [["$$", "$$"]],
+        tags: 'ams',
+        macros: {
+            // Functions
+            arccot: '\\operatorname{arccot}',
+            arcsec: '\\operatorname{arcsec}',
+            arccsc: '\\operatorname{arccsc}',
+        },
+    },
+    svg: { fontCache: 'global' }
+}
---
+++ js/main.js
@@ -0,0 +1,36 @@
+function reset () {
+    let r = window.editormd.$marked.Renderer;
+    let s = window.editormd.$marked.setOptions;
+    window.editormd.$marked = function (src, opt, callback) { return rs_render(src) };
+    window.editormd.$marked.Renderer = r;
+    window.editormd.$marked.setOptions = s;
+}
+
+
+const config = {
+    width: "100%",
+    height: "100%",
+    delay: 100,//ms
+    syncScrolling: "single",
+    path: "https://cdn.jsdelivr.net/npm/editor.md@1.5.0/lib/",
+    placeholder: "Enjoy Notedown!",
+    toolbarIcons: function () {
+        return ["undo", "redo", "|", "watch", "preview"]
+    },
+    onload: function () {
+        //console.log('onload', this);
+        this.fullscreen();
+        this.unwatch()
+        reset()
+        this.watch()
+        MathJax.typeset()
+    },
+    onchange: function () {
+        MathJax.typeset()
+    }
+}
+
+
+
+$(function () { editormd("notedown_editor", config) });
+
